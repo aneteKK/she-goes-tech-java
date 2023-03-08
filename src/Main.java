c2085c6077d303094cddd3eef7ea18a439a916b3
Anete Koknevica <anete.koknevica@gmail.com>
2023-03-08 10:02:01 +0200

    got int a using JOptionPane.showOptionDialog

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+
+
+        public class Main {
+
+            int a;
+            int b;
+            String operator;
+            public static void main(String[] args) {
+
+                String[] options = { "/","x","-","+","0","9","8","7","6","5","4","3","2","1"};
+                var selection = JOptionPane.showOptionDialog(null, "Select one:", "What do you want to do?",
+                        JOptionPane.INFORMATION_MESSAGE, 3, null, options, options[0]);
+
+                if (selection == 0) {
+                    System.out.println("You set a to: " a );
+
+                }
+                if (selection == 1) {
+                    System.out.println("You set a to: " a );
+
+                }
+                if (selection == 2) {
+                    System.out.println("You set a to: " a );
+
+                }
+                if (selection == 3) {
+                    System.out.println("You set a to: " a );
+
+                }
+                if (selection == 4) {
+                   int a = 0;
+                    System.out.println("You set a to: " a );
+                }
+                if (selection == 5) {
+                    int a = 1;
+                    System.out.println("You set a to: " a );
+                }
+                if (selection == 6) {
+                    int a = 2;
+                    System.out.println("You set a to: " a );
+                }
+                if (selection == 7) {
+                    int a = 3;
+                    System.out.println("You set a to: " a );
+                }
+                if (selection == 8) {
+                    int a = 4;
+                    System.out.println("You set a to: " a );
+                }if (selection == 9) {
+                    int a = 5;
+                    System.out.println("You set a to: " a );
+                }if (selection == 10) {
+                    int a = 6;
+                    System.out.println("You set a to: " a );
+                }if (selection == 11) {
+                    int a = 7;
+                    System.out.println("You set a to: " a );
+                }if (selection == 12) {
+                    int a = 8;
+                    System.out.println("You set a to: " a );
+                }if (selection == 13) {
+                    int a = 9;
+                    System.out.println("You set a to: " a );
+                }
+
+
+            }
+        }

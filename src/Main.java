d5f50664928209e00e408fd442cba8cb65609c93
Anete Koknevica <anete.koknevica@gmail.com>
2023-03-08 11:04:43 +0200

    got int a, int b, made calculator method

--- src/Main.java
+++ src/Main.java
@@ -3,67 +3,150 @@ import javax.swing.*;
 
         public class Main {
 
-            int a;
-            int b;
-            String operator;
             public static void main(String[] args) {
 
-                String[] options = { "/","x","-","+","0","9","8","7","6","5","4","3","2","1"};
-                var selection = JOptionPane.showOptionDialog(null, "Select one:", "What do you want to do?",
-                        JOptionPane.INFORMATION_MESSAGE, 3, null, options, options[0]);
+                String[] optionsA = {"0", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
+                var selection1 = JOptionPane.showOptionDialog(null, "Select one:", "What do you want to do?",
+                        JOptionPane.INFORMATION_MESSAGE, 3, null, optionsA, optionsA[0]);
 
-                if (selection == 0) {
-                    System.out.println("You set a to: " a );
 
+                if (selection1 == 0) {
+                    int a = 0;
+                    System.out.println("You set a to: 0");
                 }
-                if (selection == 1) {
-                    System.out.println("You set a to: " a );
-
-                }
-                if (selection == 2) {
-                    System.out.println("You set a to: " a );
-
-                }
-                if (selection == 3) {
-                    System.out.println("You set a to: " a );
-
-                }
-                if (selection == 4) {
-                   int a = 0;
-                    System.out.println("You set a to: " a );
-                }
-                if (selection == 5) {
-                    int a = 1;
-                    System.out.println("You set a to: " a );
-                }
-                if (selection == 6) {
-                    int a = 2;
-                    System.out.println("You set a to: " a );
-                }
-                if (selection == 7) {
-                    int a = 3;
-                    System.out.println("You set a to: " a );
-                }
-                if (selection == 8) {
-                    int a = 4;
-                    System.out.println("You set a to: " a );
-                }if (selection == 9) {
-                    int a = 5;
-                    System.out.println("You set a to: " a );
-                }if (selection == 10) {
-                    int a = 6;
-                    System.out.println("You set a to: " a );
-                }if (selection == 11) {
-                    int a = 7;
-                    System.out.println("You set a to: " a );
-                }if (selection == 12) {
-                    int a = 8;
-                    System.out.println("You set a to: " a );
-                }if (selection == 13) {
+                if (selection1 == 1) {
                     int a = 9;
-                    System.out.println("You set a to: " a );
+                    System.out.println("You set a to: 9");
+                }
+                if (selection1 == 2) {
+                    int a = 8;
+                    System.out.println("You set a to: 8");
+                }
+                if (selection1 == 3) {
+                    int a = 7;
+                    System.out.println("You set a to: 7");
+                }
+                if (selection1 == 4) {
+                    int a = 6;
+                    System.out.println("You set a to: 6");
+                }
+                if (selection1 == 5) {
+                    int a = 5;
+                    System.out.println("You set a to: 5");
+                }
+                if (selection1 == 6) {
+                    int a = 4;
+                    System.out.println("You set a to: 4");
+                }
+                if (selection1 == 7) {
+                    int a = 3;
+                    System.out.println("You set a to: 3");
+                }
+                if (selection1 == 8) {
+                    int a = 2;
+                    System.out.println("You set a to: 2");
+                }
+                //if (selection1 == 9) {
+                //  int a = 1;
+                //System.out.println("You set a to: " a );
+                //}
+
+
+                String[] operatorsC = {"/", "x", "-", "+"};
+                var selection2 = JOptionPane.showOptionDialog(null, "Select one:", "What do you want to do?",
+                        JOptionPane.INFORMATION_MESSAGE, 3, null, operatorsC, operatorsC[0]);
+
+                if (selection2 == 0) {
+                    String operator = "/";
+                    System.out.println("You set operator to '/'");
+                }
+                if (selection2 == 1) {
+                    String operator = "x";
+                    System.out.println("You set operator to 'x'");
+                }
+                if (selection2 == 2) {
+                    String operator = "-";
+                    System.out.println("You set operator to '-'");
+                }
+                if (selection2 == 3) {
+                    String operator = "+";
+                    System.out.println("You set operator to '+'");
+
                 }
 
 
+                String[] optionsB = {"0", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
+                var selection3 = JOptionPane.showOptionDialog(null, "Select one:", "What do you want to do?",
+                        JOptionPane.INFORMATION_MESSAGE, 3, null, optionsB, optionsB[0]);
+
+
+                if (selection3 == 0) {
+                    int b = 0;
+                    System.out.println("You set a to: 0");
+                }
+                if (selection3 == 1) {
+                    int b = 9;
+                    System.out.println("You set a to: 9");
+                }
+                if (selection3 == 2) {
+                    int b = 8;
+                    System.out.println("You set a to: 8");
+                }
+                if (selection3 == 3) {
+                    int b = 7;
+                    System.out.println("You set a to: 7");
+                }
+                if (selection3 == 4) {
+                    int b = 6;
+                    System.out.println("You set a to: 6");
+                }
+                if (selection3 == 5) {
+                    int b = 5;
+                    System.out.println("You set a to: 5");
+                }
+                if (selection3 == 6) {
+                    int b = 4;
+                    System.out.println("You set a to: 4");
+                }
+                if (selection3 == 7) {
+                    int b = 3;
+                    System.out.println("You set a to: 3");
+                }
+                if (selection3 == 8) {
+                    int b = 2;
+                    System.out.println("You set a to: 2");
+                }
+
+                //if (selection1 == 9) {
+                //  int a = 1;
+                //System.out.println("You set a to: " a );
+                //}
+
+                System.out.println(calculator(optionsA,optionsB,String operatorsC ));
+            }
+            public static double calculator(int a, int b, String operator) {
+
+
+                if ("+".equals(operator)) {
+                    System.out.println("You are adding");
+                    return a + b;
+                }
+                if ("-".equals(operator)) {
+                    System.out.println("You are subtracting");
+                    return a - b;
+                }
+
+                if ("*".equals(operator)) {
+                    System.out.println("You are multiplying");
+                    return a * b;
+                }
+                if ("/".equals(operator)) {
+                    System.out.println("You are dividing");
+                    return a / b;
+                }
+                if (operator == "%") {
+                    return a % b;
+                }
+                return 0;
             }
         }
---
+++ src/calculator.java
@@ -0,0 +1,44 @@
+public class calculator{
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int sum;
+        double quotient;
+        int product;
+        int difference;
+
+        System.out.println(calculator(1, 2, "-"));
+        System.out.println(calculator(1, 5, "+"));
+        System.out.println(calculator(6, 2, "*"));
+        System.out.println(calculator(1, 8, "/"));
+        System.out.println(calculator(7, 3, "%"));
+
+    }
+
+    public static double calculator(int a, int b, String operator) {
+        System.out.println("+".charAt(0));
+        System.out.println(operator.charAt(0));
+
+        if ("+".equals(operator)) {
+            System.out.println("You are adding");
+            return a + b;
+        }
+        if ("-".equals(operator)) {
+            System.out.println("You are subtracting");
+            return a - b;
+        }
+
+        if ("*".equals(operator)) {
+            System.out.println("You are multiplying");
+            return a * b;
+        }
+        if ("/".equals(operator)) {
+            System.out.println("You are dividing");
+            return a / b;
+        }
+        if (operator == "%") {
+            return a % b;
+        }
+        return 0;
+    }
+}

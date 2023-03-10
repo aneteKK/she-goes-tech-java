573053d7793af5590ef8f15c7d0eb69dff660b0b
Anete Koknevica <anete.koknevica@gmail.com>
2023-03-10 21:19:53 +0200

    info about literals

---
+++ src/com/sda/she_likes_java/operators/Literals.java
@@ -0,0 +1,24 @@
+package com.sda.she_likes_java.operators;
+
+public class Literals {
+
+    public static void main(String[] args) {
+        //literal is direct value
+        //String literal is e.g. "name"
+        String name = "Anete";
+
+        //integer literal
+        int age = 30; //30 is of type int
+
+        //long literal
+        long  anotherAge = 30L; //need to add 'L' after number
+
+        //double literal
+        double doubleNumber = 3.1; //no need to add anything
+
+        //float literal
+        float floatNumber = 3.1F; // need to add 'f' after number
+
+        System.out.println(doubleNumber +"+"+ floatNumber);
+    }
+}

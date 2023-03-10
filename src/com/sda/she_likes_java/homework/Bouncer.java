1ab2b4834498437d92b86b38c8ee1e243dbeb515
Anete Koknevica <anete.koknevica@gmail.com>
2023-03-10 18:44:48 +0200

    made bouncer for nightclub, added height with if statement with 'or'

--- src/com/sda/she_likes_java/homework/Bouncer.java
+++ src/com/sda/she_likes_java/homework/Bouncer.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 public class Bouncer {
     int age;
     static boolean isKid;
+    static boolean isShort;
 
     public static void main(String[] args) {
         Scanner myAge = new Scanner(System.in);
         System.out.println("Enter age");
         int age = myAge.nextInt();
 
-        if (age < 18) {
+        Scanner myHeight = new Scanner(System.in);
+        System.out.println("Enter height");
+        int height = myHeight.nextInt();
+
+
+        if (age < 16 || height < 155) {
             isKid = true;
+            isShort = true;
             System.out.println("You cannot enter!");
         } else {
             isKid = false;
+            isShort = false;
             System.out.println("Come inside!");
 
 

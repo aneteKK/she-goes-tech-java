55010f7c362c856b79258e1ea8f06b40fd6fbd46
Anete Koknevica <anete.koknevica@gmail.com>
2023-03-22 10:07:08 +0200

    homework for 22.03.

--- src/com/sda/she_likes_java/arrays/ArrayDemo.java
+++ src/com/sda/she_likes_java/arrays/ArrayDemo.java
@@ -3,7 +3,7 @@ package com.sda.she_likes_java.arrays;
 public class ArrayDemo {
     public static void main(String[] args) {
 
-        // create an array (set) of 5 values
+        // create an array (set) of 100 values
         int[] numbers = new int[100];
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index*3;

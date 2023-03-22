22e5f6b56c02a35740320a2b034a1c633b03ab2d
Anete Koknevica <anete.koknevica@gmail.com>
2023-03-22 20:44:41 +0200

    string concatenation 3 ways

---
+++ src/com/sda/she_likes_java/strings/StringsExample.java
@@ -0,0 +1,18 @@
+package com.sda.she_likes_java.strings;
+
+public class StringsExample {
+
+    public static void main(String[] args) {
+
+
+        String name = "Anete";
+        String surname = "Koknevica";
+        int age = 30;
+
+        String info = " Hi, I'm " + name + " " + surname + ". " + "I am " + age + " years old.";
+        String info2 = String.format(" Hi, I'm %s %s. I am %s years old.",name, surname, age);
+        System.out.println(info);
+        System.out.println(info2);
+        System.out.println(" Hi, I'm %s %s. I am %s old.".formatted(name, surname, age));
+    }
+}

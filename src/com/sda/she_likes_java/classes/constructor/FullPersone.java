b9044b28fe9e784db66c23593acea425a920c823
Anete Koknevica <anete.koknevica@gmail.com>
2023-03-27 20:53:19 +0300

    Full person with constructor

---
+++ src/com/sda/she_likes_java/classes/constructor/FullPersone.java
@@ -0,0 +1,41 @@
+package com.sda.she_likes_java.classes.constructor;
+
+public class FullPersone {
+
+    String name;
+    String surname;
+    int age;
+    FullAddress address;
+
+
+    public FullPersone() {
+    }
+
+    public FullPersone(String name, String surname, int age, FullAddress address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+    @Override
+    public String toString() {
+        return "FullPersone{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address=" + address +
+                '}';
+    }
+    public static void main(String[] args) {
+        FullAddress address = new FullAddress("Latvia", "Riga", "Liela iela 3", "Lv2015");
+        FullPersone personWithConstructor = new FullPersone("Anete", "Koknevica", 30, address);
+        System.out.println("Person is : "+ personWithConstructor);
+
+
+
+    }
+
+
+
+
+}

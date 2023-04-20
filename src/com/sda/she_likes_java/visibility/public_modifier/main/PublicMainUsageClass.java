752a5ac250ff81a312f6af1f3311d58463b0d0a1
Anete Koknevica <anete.koknevica@gmail.com>
2023-04-20 15:16:12 +0300

    public modifier introduction

---
+++ src/com/sda/she_likes_java/visibility/public_modifier/main/PublicMainUsageClass.java
@@ -0,0 +1,11 @@
+package com.sda.she_likes_java.visibility.public_modifier.main;
+
+import com.sda.she_likes_java.visibility.public_modifier.drivers.ProfessionalDriver;
+
+public class PublicMainUsageClass {
+
+    public static void main(String[] args) {
+        ProfessionalDriver me = new ProfessionalDriver();
+
+    }
+}

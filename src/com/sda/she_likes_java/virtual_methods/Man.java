f8a91189c9a8c32e4671159093117b774be09cc0
Anete Koknevica <anete.koknevica@gmail.com>
2023-04-24 19:26:10 +0300

    virtual methods , added Man and Woman classes

---
+++ src/com/sda/she_likes_java/virtual_methods/Man.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.virtual_methods;
+
+public class Man extends Human{
+    @Override
+    public void whoYouAre() {
+        System.out.println(" I'm a man");
+    }
+
+    public void learnToCleanHouse(){
+        System.out.println(" I like to learn housework");}
+}
+}

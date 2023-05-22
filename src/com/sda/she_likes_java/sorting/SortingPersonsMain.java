a32a92e2feafb60e695cdc230df83553e0b02039
Anete Koknevica <anete.koknevica@gmail.com>
2023-05-22 11:22:23 +0300

    Compound comparator with methods provided by Java : reversed(..)  and thenCompairing(..)

--- src/com/sda/she_likes_java/sorting/SortingPersonsMain.java
+++ src/com/sda/she_likes_java/sorting/SortingPersonsMain.java
@@ -2,6 +2,7 @@ package com.sda.she_likes_java.sorting;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingPersonsMain {
@@ -10,12 +11,16 @@ public class SortingPersonsMain {
         SortedPerson olaf = new SortedPerson("Olaf", 22);
         SortedPerson gustaw = new SortedPerson("Gustaw", 11);
         SortedPerson zorro = new SortedPerson("Zorro", 11);
+        SortedPerson zorro1 = new SortedPerson("Zorro", 7);
+        SortedPerson zorra = new SortedPerson("Zorra", 11);
         List<SortedPerson> persons = new ArrayList<>();
         persons.add(olaf);
         persons.add(gustaw);
         persons.add(zorro);
         persons.add(zorro);
         persons.add(zorro);
+        persons.add(zorra);
+        persons.add(zorro1);
 
         Collections.sort(persons);
         System.out.println("After sorting: " + persons);
@@ -24,23 +29,25 @@ public class SortingPersonsMain {
         // if returns 1 that means: gustaw, olaf
 //        System.out.println("olaf.compareTo(gustaw): " + olaf.compareTo(gustaw));
 //        System.out.println("gustaw vs zorro: " + gustaw.compareTo(zorro));
-
-        Collections.sort(persons, new AgedReversedOrderOfSortedPersons());
-        System.out.println("Reversed order based on age " +  persons);
+        Comparator<SortedPerson> reversedAgeOrder = new AgedReversedOrderOfSortedPersons();
+        Collections.sort(persons, reversedAgeOrder);
+        System.out.println("Reversed order based on age: " + persons);
 
         List<String> names = new ArrayList<>();
-        names.add("Emilija");
-        names.add("Herberts");
-        names.add("Roberts");
-        names.add("Anete");
+        names.add("Ramune");
+        names.add("Lina");
+        names.add("Alma");
+        names.add("Karolina");
         Collections.sort(names);
-
-        System.out.println("Sorted names : " + names);
+        System.out.println("sorted names: " + names);
 
         Collections.sort(persons, new LexicalOrderForSortedPerson());
-        System.out.println("Persons sorted by lexical order: " + persons);
+        System.out.println("persons sorted by name: " + persons);
 
         Collections.sort(persons, new ReversedLexicalOrderForSortedPerson());
-        System.out.println("Person sorted in reversed order by name: " + persons);
+        System.out.println("persons sorted by name - reversed: " + persons);
+
+        Collections.sort(persons,
+                new LexicalOrderForSortedPerson().reversed().thenComparing(reversedAgeOrder));
     }
 }

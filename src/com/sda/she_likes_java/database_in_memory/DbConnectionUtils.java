49cdf0e233babafd50dd7f5091518fee24e36dc2
Anete Koknevica <anete.koknevica@gmail.com>
2023-05-31 20:38:05 +0300

    h2 memory started example

---
+++ src/com/sda/she_likes_java/database_in_memory/DbConnectionUtils.java
@@ -0,0 +1,19 @@
+package com.sda.she_likes_java.database_in_memory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnectionUtils {
+
+    public static Connection getConnection(String dbUrl, String userName, String password){
+        try {
+            return DriverManager.getConnection(dbUrl, userName, password);
+        } catch (SQLException e) {
+
+            System.out.println("Exeption during database connestion " + e);
+            return null;
+        }
+
+    }
+}
